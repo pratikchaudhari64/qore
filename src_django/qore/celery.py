769a636682efd2a3666ad57a4af537bfab2fde12
Oracle Public Cloud User <opc@test-instance-arm-alwaysfreeoption.sub09191221560.testvcn.oraclevcn.com>
2025-11-19 17:22:34 +0000

    celery app

---
+++ src_django/qore/celery.py
@@ -0,0 +1,13 @@
+import os
+from celery import Celery
+
+# Set default Django settings module
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qore.settings')
+
+app = Celery('qore')
+
+# Load config from Django settings with CELERY_ prefix
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Auto-discover tasks from all installed apps
+app.autodiscover_tasks()
